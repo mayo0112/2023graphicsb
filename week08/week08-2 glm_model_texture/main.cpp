27c279ab03fab51918d2129c4370a231b737e95b
mayo <91170816+mayo0112@users.noreply.github.com>
2023-04-06 16:46:08 +0800

    Add files via upload
    第八週的程式

---
+++ week08/week08-2 glm_model_texture/main.cpp
@@ -0,0 +1,43 @@
+#include <GL/glut.h>
+#include <opencv/highgui.h>
+#include <opencv/cv.h>
+#include <GL/glut.h>
+int myTexture(char * filename)
+{
+    IplImage * img = cvLoadImage(filename);
+    cvCvtColor(img,img, CV_BGR2RGB);
+    glEnable(GL_TEXTURE_2D);
+    GLuint id;
+    glGenTextures(1, &id);
+    glBindTexture(GL_TEXTURE_2D, id);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, img->width, img->height, 0, GL_RGB, GL_UNSIGNED_BYTE, img->imageData);
+    return id;
+}
+#include "glm.h"
+GLMmodel * pmodel = NULL;
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    if(pmodel==NULL){
+        pmodel= glmReadOBJ("Gundam.obj");
+        glmUnitize(pmodel);
+        glmFacetNormals(pmodel);
+        glmVertexNormals(pmodel,90);
+    }
+    glmDraw(pmodel,GLM_SMOOTH | GLM_MATERIAL);
+    glutSwapBuffers();
+}
+int main(int argc, char *argv[])
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+    glutCreateWindow("week08");
+    myTexture("Diffuse.jpg");
+    glutDisplayFunc(display);
+    glutIdleFunc(display);
+    glutMainLoop();
+}
