9fe2b81a2c9e1ee845069722330bc4c7d69921a5
unknown <h0965414829@gmail.com>
2023-06-06 12:51:36 +0800

    week16

--- Final_Project/main.cpp
+++ Final_Project/main.cpp
@@ -1,61 +1,64 @@
-#include <stdio.h>///要檔案的Input/Output
+///week15_03_03 要把我們的關節angle,變成陣列
+/// float angle; 改成 float angle[20] = {} ;
+///之後的 angle[0], angle[1] angle[2]
+/// motion裡, 有改變 angle值, 換成 angle[ID]
+
+///Final_Project 之後都用同一個程式,來進行 Final Project
+#include <stdio.h> ///要檔案的Input/Output
 #include <GL/glut.h>
-#include "glm.h"///再把glm.cpp 在左邊 Add files 加進去
-GLMmodel * head = NULL;
-GLMmodel * body = NULL;
+#include "glm.h" ///week13_step02-2
+///week13_step02-2 再把 glm.cpp 在左邊 Add files 加進去
+GLMmodel * head = NULL;///week13_step02-2
+GLMmodel * body = NULL;///week13_step02-2
 GLMmodel * uparmR = NULL;
 GLMmodel * lowarmR = NULL;
-
-int show[4] = {1,1,1,1};///week14_step03-1 都秀出來
-int ID = 3;///week14_step03-1 設定關節 ID 0:頭 1:身體 2:上手臂 3:下手臂
-float teapotX=0,teapotY=0;
-float angle[20]={};///Week15-3_step03-1
-///float angle=0, angle2=0, angle3=0;///week14_step03-2
-FILE * fout = NULL;
-FILE * fin = NULL;
-
-void keyboard(unsigned char key,int x,int y)
-{
-    if(key=='0') ID = 0; ///week14_step03-1 ///show[0] = ! show[0];
-    if(key=='1') ID = 1; ///week14_step03-1 ///show[1] = ! show[1];
-    if(key=='2') ID = 2; ///week14_step03-1 ///show[2] = ! show[2];
-    if(key=='3') ID = 3; ///week14_step03-1 ///show[3] = ! show[3];
-    glutPostRedisplay();
-}
+int show[4] = {1,1,1,1}; ///week14_step03_1 都秀出來 ///week13_step03-1
+int ID = 3; ///week14_step03_1 設定關節 ID
+float teapotX = 0, teapotY = 0;
+float angle[20] = {};///week15_03_03
+///float angle = 0;
+FILE * fout = NULL;///step02-1
+FILE * fin = NULL;///step02-2
+void keyboard(unsigned char key, int x, int y) {///week13_step03-1
+    if(key=='0') ID = 0; ///week14_step03_1 ///show[0] = ! show[0];///week13_step03-1
+    if(key=='1') ID = 1; ///week14_step03_1 ///show[1] = ! show[1];///week13_step03-1
+    if(key=='2') ID = 2; ///week14_step03_1 ///show[2] = ! show[2];///week13_step03-1
+    if(key=='3') ID = 3; ///week14_step03_1 ///show[3] = ! show[3];///week13_step03-1
+    glutPostRedisplay();///week13_step03-1
+}///week13_step03-1
 void display()
 {
     glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
-    if(head==NULL)
-    {
-        head = glmReadOBJ("model/head.obj");
+    if(head==NULL){///week13_step02-2
+        head = glmReadOBJ("model/head.obj");///week13_step02-2
         body = glmReadOBJ("model/body.obj");
         uparmR = glmReadOBJ("model/uparmR.obj");
         lowarmR = glmReadOBJ("model/lowarmR.obj");
-        ///glmUnitize(head);
-    }
+        ///glmUnitize(head); ///week13_step02-2 之後會改
+    }///week13_step02-2
     glPushMatrix();
-        glScalef(0.3,0.3,0.3);
-        glPushMatrix();
-            ///glTranslatef(teapotX,teapotY,0);
+        glScalef(0.3, 0.3, 0.3);///week13_step02-3
+        glPushMatrix();///week13_step03-2
+            ///glTranslatef(teapotX, teapotY, 0); ///week13_step03-2
 
-            if(ID==0) glColor3f(1,0,0); ///week14_step03-1 紅色
-            else glColor3f(1,1,1); ///week14_step03-1 白色
-            if(show[0]) glmDraw(head,GLM_MATERIAL);
-        glPopMatrix();
+            if(ID==0) glColor3f(1,0,0);///week14_step03_1 秀紅色
+            else glColor3f(1,1,1);///week14_step03_1 秀白色
+            if(show[0]) glmDraw(head, GLM_MATERIAL);///week13_step02-3
+        glPopMatrix();///week13_step03-2
 
-        if(ID==1) glColor3f(1,0,0); ///week14_step03-1 紅色
-        else glColor3f(1,1,1); ///week14_step03-1 白色
-        if(show[1]) glmDraw(body,GLM_MATERIAL);
+        if(ID==1) glColor3f(1,0,0);///week14_step03_1 秀紅色
+        else glColor3f(1,1,1);///week14_step03_1 秀白色
+        if(show[1]) glmDraw(body, GLM_MATERIAL);///week13_step02-2
 
         glPushMatrix(); ///week14_step03_2
             ///glTranslatef(teapotX, teapotY, 0); ///week14_step03_2 要設定 TRT
-            glTranslatef(-1.360000, +0.360000, 0); ///week14_step03_2
+            glTranslatef(-1.360000, +0.500000, 0); ///week14_step03_2
             glRotatef(angle[2], 0, 0, 1); ///week14_step03_2
-            glTranslatef(1.360000, -0.360000, 0); ///week14_step03_2
+            glTranslatef(1.360000, -0.500000, 0); ///week14_step03_2
 
-            if(ID==2) glColor3f(1,0,0); ///week14_step03-1 紅色
-            else glColor3f(1,1,1); ///week14_step03-1 白色
-            if(show[2]) glmDraw(uparmR,GLM_MATERIAL);
+            if(ID==2) glColor3f(1,0,0);///week14_step03_1 秀紅色
+            else glColor3f(1,1,1);///week14_step03_1 秀白色
+            if(show[2]) glmDraw(uparmR, GLM_MATERIAL);///week13_step02-3
 
             glPushMatrix();  ///week14_step03_2
                 ///glTranslatef(teapotX, teapotY, 0); ///week14_step03_2 要設定 TRT
@@ -63,46 +66,41 @@ void display()
                 glRotatef(angle[3], 0, 0, 1);
                 glTranslatef(1.959999, -0.080000, 0);
 
-                if(ID==3) glColor3f(1,0,0); ///week14_step03-1 紅色
-                else glColor3f(1,1,1); ///week14_step03-1 白色
-                if(show[3]) glmDraw(lowarmR,GLM_MATERIAL);
-
+                if(ID==3) glColor3f(1,0,0);///week14_step03_1 秀紅色
+                else glColor3f(1,1,1);///week14_step03_1 秀白色
+                if(show[3]) glmDraw(lowarmR, GLM_MATERIAL);///week13_step02-3
             glPopMatrix();  ///week14_step03_2
-
         glPopMatrix(); ///week14_step03_2
 
-    glPopMatrix();
 
-    glColor3f(0,1,0);///week14_step03_2 綠色的
-    glutSolidTeapot(0.02);///設小茶壺作為中心點
+    glPopMatrix();
+    glColor3f(0,1,0);  ///week14_step03_2 綠色的
+    glutSolidTeapot( 0.02 ); ///week14_step03_2
     glutSwapBuffers();
 }
-int oldX=0, oldY=0;
-void mouse(int button,int state,int x,int y)
-{
-    if(state==GLUT_DOWN)
-    {
+int oldX=0, oldY=0; ///week13_step03-2
+void mouse(int button, int state, int x, int y) {
+    if(state==GLUT_DOWN){
         oldX = x;
         oldY = y;
     }
 }
-void motion(int x,int y)
-{
+void motion(int x, int y) { ///week13_step03-2
     teapotX += (x - oldX)/150.0*3;
     teapotY -= (y - oldY)/150.0*3;
     printf("glTranslatef(%f, %f, 0);\n", teapotX, teapotY); ///week14_step03_2
-    angle[ID] += x-oldX; ///week15_step3-1
+    angle[ID] += x-oldX; ///week15_step03_03
     oldX = x;
     oldY = y;
     glutPostRedisplay();
 }
-int main(int argc, char**argv)
+int main(int argc, char** argv)
 {
-    glutInit( &argc, argv );
+    glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_DEPTH);
-    glutCreateWindow("week14");
+    glutCreateWindow("week12");
 
-    glutMotionFunc(motion);
+    glutMotionFunc(motion); ///week13_step03-2
     glutDisplayFunc(display);
     glutKeyboardFunc(keyboard);
     glutMouseFunc(mouse);
